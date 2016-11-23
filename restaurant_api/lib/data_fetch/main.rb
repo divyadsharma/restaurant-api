a9f00319577ac3e37cb6b63224546f81b355430a
divyadsharma <dvsharma129@gmail.com>
2016-11-23 23:26:27 +0530

    addedrestaurant api

--- restaurant_api/lib/data_fetch/main.rb
+++ restaurant_api/lib/data_fetch/main.rb
@@ -6,7 +6,6 @@ module DataFetch
     def self.fetch_restaurants
       res = File.read("#{Rails.root}/public/sample_data.json")
       res = ActiveSupport::JSON.decode(res)
-      debugger
       return res
     rescue Exception => e
       Rails.logger.info 'Bad Request' + e.message
